236d087a9d41ea09b79eaa0ccafa212ed3505b94
reetuduggal <reet.r.duggal@gmail.com>
2013-01-01 04:33:34 -0800

    Update controllers/email.php

--- controllers/email.php
+++ controllers/email.php
@@ -6,7 +6,7 @@ class Email extends Z_Controller {
     parent::__construct();
     $this->load->model('Message_model');
   }
-  // have a nice day
+  // have a nice day helloooooooooooooooooooooo
   new line  add
   public function inbox($label_id = 0, $message_id = 0) {
     $this->emailfunctions->gatekeeper();
